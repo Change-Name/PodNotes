a458a539c28535de4d19808ff45ca9702aa9572c
N0Name <53141446+Change-Name@users.noreply.github.com>
2025-05-20 21:09:40 +0200

    implemented podcast search

--- src/ui/PodcastView/PodcastView.svelte
+++ src/ui/PodcastView/PodcastView.svelte
@@ -1,278 +1,300 @@
 <script lang="ts">
-	import { PodcastFeed } from "src/types/PodcastFeed";
-	import PodcastGrid from "./PodcastGrid.svelte";
-	import {
-		currentEpisode,
-		savedFeeds,
-		episodeCache,
-		playlists,
-		queue,
-		favorites,
-		localFiles,
-		podcastView,
-		viewState,
-		downloadedEpisodes,
-	} from "src/store";
-	import EpisodePlayer from "./EpisodePlayer.svelte";
-	import EpisodeList from "./EpisodeList.svelte";
-	import { Episode } from "src/types/Episode";
-	import FeedParser from "src/parser/feedParser";
-	import TopBar from "./TopBar.svelte";
-	import { ViewState } from "src/types/ViewState";
-	import { onMount } from "svelte";
-	import EpisodeListHeader from "./EpisodeListHeader.svelte";
-	import Icon from "../obsidian/Icon.svelte";
-	import { debounce } from "obsidian";
-	import searchEpisodes from "src/utility/searchEpisodes";
-	import { Playlist } from "src/types/Playlist";
-	import spawnEpisodeContextMenu from "./spawnEpisodeContextMenu";
+    import { PodcastFeed } from "src/types/PodcastFeed";
+    import PodcastGrid from "./PodcastGrid.svelte";
+    import {
+        currentEpisode,
+        savedFeeds,
+        episodeCache,
+        playlists,
+        queue,
+        favorites,
+        localFiles,
+        podcastView,
+        viewState,
+        downloadedEpisodes,
+    } from "src/store";
+    import EpisodePlayer from "./EpisodePlayer.svelte";
+    import EpisodeList from "./EpisodeList.svelte";
+    import { Episode } from "src/types/Episode";
+    import FeedParser from "src/parser/feedParser";
+    import TopBar from "./TopBar.svelte";
+    import { ViewState } from "src/types/ViewState";
+    import { onMount } from "svelte";
+    import EpisodeListHeader from "./EpisodeListHeader.svelte";
+    import Icon from "../obsidian/Icon.svelte";
+    import { debounce } from "obsidian";
+    import searchEpisodes from "src/utility/searchEpisodes";
+    import { Playlist } from "src/types/Playlist";
+    import spawnEpisodeContextMenu from "./spawnEpisodeContextMenu";
+    import Text from "../obsidian/Text.svelte";
+    import Fuse from "fuse.js";
 
-	let feeds: PodcastFeed[] = [];
-	let selectedFeed: PodcastFeed | null = null;
-	let selectedPlaylist: Playlist | null = null;
-	let displayedEpisodes: Episode[] = [];
-	let displayedPlaylists: Playlist[] = [];
-	let latestEpisodes: Episode[] = [];
+    let feeds: PodcastFeed[] = [];
+    let selectedFeed: PodcastFeed | null = null;
+    let selectedPlaylist: Playlist | null = null;
+    let displayedEpisodes: Episode[] = [];
+    let displayedPlaylists: Playlist[] = [];
+    let latestEpisodes: Episode[] = [];
 
-	onMount(async () => {
-		const unsubscribePlaylists = playlists.subscribe((pl) => {
-			displayedPlaylists = [$queue, $favorites, $localFiles, ...Object.values(pl)];
-		});
+    let podcastSearchQuery = "";
+    $: filteredFeeds = podcastSearchQuery.length
+        ? new Fuse(feeds, {
+              shouldSort: true,
+              findAllMatches: true,
+              threshold: 0.4,
+              isCaseSensitive: false,
+              keys: ['title']
+          })
+              .search(podcastSearchQuery)
+              .map(r => r.item)
+        : feeds;
 
-		const unsubscribeSavedFeeds = savedFeeds.subscribe((storeValue) => {
-			feeds = Object.values(storeValue);
-		});
+    onMount(async () => {
+        const unsubscribePlaylists = playlists.subscribe((pl) => {
+            displayedPlaylists = [$queue, $favorites, $localFiles, ...Object.values(pl)];
+        });
 
-		await fetchEpisodesInAllFeeds(feeds);
+        const unsubscribeSavedFeeds = savedFeeds.subscribe((storeValue) => {
+            feeds = Object.values(storeValue);
+        });
 
-		const unsubscribeEpisodeCache = episodeCache.subscribe((cache) => {
-			latestEpisodes = Object.entries(cache)
-				.map(([_, episodes]) => episodes.slice(0, 10))
-				.flat()
-				.sort((a, b) => {
-					if (a.episodeDate && b.episodeDate)
-						return Number(b.episodeDate) - Number(a.episodeDate);
+        await fetchEpisodesInAllFeeds(feeds);
 
-					return 0;
-				});
-		});
+        const unsubscribeEpisodeCache = episodeCache.subscribe((cache) => {
+            latestEpisodes = Object.entries(cache)
+                .map(([_, episodes]) => episodes.slice(0, 10))
+                .flat()
+                .sort((a, b) => {
+                    if (a.episodeDate && b.episodeDate)
+                        return Number(b.episodeDate) - Number(a.episodeDate);
 
-		if (!selectedFeed) {
-			displayedEpisodes = latestEpisodes;
-		}
+                    return 0;
+                });
+        });
 
-		return () => {
-			unsubscribeEpisodeCache();
-			unsubscribeSavedFeeds();
-			unsubscribePlaylists();
-		};
-	});
+        if (!selectedFeed) {
+            displayedEpisodes = latestEpisodes;
+        }
 
-	async function fetchEpisodes(
-		feed: PodcastFeed,
-		useCache: boolean = true
-	): Promise<Episode[]> {
-		const cachedEpisodesInFeed = $episodeCache[feed.title];
-		
-		if (
-			useCache &&
-			cachedEpisodesInFeed &&
-			cachedEpisodesInFeed.length > 0
-		) {
-			return cachedEpisodesInFeed;
-		}
+        return () => {
+            unsubscribeEpisodeCache();
+            unsubscribeSavedFeeds();
+            unsubscribePlaylists();
+        };
+    });
 
-		try {
-			const episodes = await new FeedParser(feed).getEpisodes(feed.url);
+    async function fetchEpisodes(
+        feed: PodcastFeed,
+        useCache: boolean = true
+    ): Promise<Episode[]> {
+        const cachedEpisodesInFeed = $episodeCache[feed.title];
+        
+        if (
+            useCache &&
+            cachedEpisodesInFeed &&
+            cachedEpisodesInFeed.length > 0
+        ) {
+            return cachedEpisodesInFeed;
+        }
 
-			episodeCache.update((cache) => ({
-				...cache,
-				[feed.title]: episodes,
-			}));
+        try {
+            const episodes = await new FeedParser(feed).getEpisodes(feed.url);
 
-			return episodes;
-		} catch (error) {
-			return $downloadedEpisodes[feed.title];
-		}
-	}
+            episodeCache.update((cache) => ({
+                ...cache,
+                [feed.title]: episodes,
+            }));
 
-	function fetchEpisodesInAllFeeds(
-		feedsToSearch: PodcastFeed[]
-	): Promise<Episode[]> {
-		return Promise.all(
-			feedsToSearch.map((feed) => fetchEpisodes(feed))
-		).then((episodes) => {
-			return episodes.flat();
-		});
-	}
+            return episodes;
+        } catch (error) {
+            return $downloadedEpisodes[feed.title];
+        }
+    }
 
-	async function handleClickPodcast(
-		event: CustomEvent<{ feed: PodcastFeed }>
-	) {
-		const { feed } = event.detail;
-		displayedEpisodes = [];
+    function fetchEpisodesInAllFeeds(
+        feedsToSearch: PodcastFeed[]
+    ): Promise<Episode[]> {
+        return Promise.all(
+            feedsToSearch.map((feed) => fetchEpisodes(feed))
+        ).then((episodes) => {
+            return episodes.flat();
+        });
+    }
 
-		selectedFeed = feed;
-		displayedEpisodes = await fetchEpisodes(feed);
-		viewState.set(ViewState.EpisodeList);
-	}
+    async function handleClickPodcast(
+        event: CustomEvent<{ feed: PodcastFeed }>
+    ) {
+        const { feed } = event.detail;
+        displayedEpisodes = [];
 
-	function handleClickEpisode(event: CustomEvent<{ episode: Episode }>) {
-		const { episode } = event.detail;
-		currentEpisode.set(episode);
+        selectedFeed = feed;
+        displayedEpisodes = await fetchEpisodes(feed);
+        viewState.set(ViewState.EpisodeList);
+    }
 
-		viewState.set(ViewState.Player);
-	}
+    function handleClickEpisode(event: CustomEvent<{ episode: Episode }>) {
+        const { episode } = event.detail;
+        currentEpisode.set(episode);
 
-	function handleContextMenuEpisode({
-		detail: { event, episode },
-	}: CustomEvent<{ episode: Episode; event: MouseEvent }>) {
-		spawnEpisodeContextMenu(episode, event);
-	}
+        viewState.set(ViewState.Player);
+    }
 
-	async function handleClickRefresh() {
-		if (!selectedFeed) return;
+    function handleContextMenuEpisode({
+        detail: { event, episode },
+    }: CustomEvent<{ episode: Episode; event: MouseEvent }>) {
+        spawnEpisodeContextMenu(episode, event);
+    }
 
-		displayedEpisodes = await fetchEpisodes(selectedFeed, false);
-	}
+    async function handleClickRefresh() {
+        if (!selectedFeed) return;
 
-	const handleSearch = debounce((event: CustomEvent<{ query: string }>) => {
-		const { query } = event.detail;
+        displayedEpisodes = await fetchEpisodes(selectedFeed, false);
+    }
 
-		if (selectedFeed) {
-			const episodesInFeed = $episodeCache[selectedFeed.title];
-			displayedEpisodes = searchEpisodes(query, episodesInFeed);
-			return;
-		}
+    const handleSearch = debounce((event: CustomEvent<{ query: string }>) => {
+        const { query } = event.detail;
 
-		displayedEpisodes = searchEpisodes(query, latestEpisodes);
-	}, 250);
+        if (selectedFeed) {
+            const episodesInFeed = $episodeCache[selectedFeed.title];
+            displayedEpisodes = searchEpisodes(query, episodesInFeed);
+            return;
+        }
 
-	function handleClickPlaylist(
-		event: CustomEvent<{ event: MouseEvent; playlist: Playlist }>
-	) {
-		const { event: clickEvent, playlist } = event.detail;
+        displayedEpisodes = searchEpisodes(query, latestEpisodes);
+    }, 250);
 
-		if (playlist.name === $queue.name && $queue.episodes.length > 0) {
-			// Only need to set the current episode if there isn't any.
-			// The current episode _is_ the front of the queue.
-			if (!$currentEpisode) {
-				currentEpisode.set($queue.episodes[0]);
-			}
+    function handleClickPlaylist(
+        event: CustomEvent<{ event: MouseEvent; playlist: Playlist }>
+    ) {
+        const { event: clickEvent, playlist } = event.detail;
 
-			viewState.set(ViewState.Player);
-		} else {
-			selectedPlaylist = playlist;
-			displayedEpisodes = playlist.episodes;
+        if (playlist.name === $queue.name && $queue.episodes.length > 0) {
+            // Only need to set the current episode if there isn't any.
+            // The current episode _is_ the front of the queue.
+            if (!$currentEpisode) {
+                currentEpisode.set($queue.episodes[0]);
+            }
 
-			viewState.set(ViewState.EpisodeList);
-		}
-	}
+            viewState.set(ViewState.Player);
+        } else {
+            selectedPlaylist = playlist;
+            displayedEpisodes = playlist.episodes;
+
+            viewState.set(ViewState.EpisodeList);
+        }
+    }
 </script>
 
 <div class="podcast-view" bind:this={$podcastView}>
-	<TopBar
-		bind:viewState={$viewState}
-		canShowEpisodeList={true}
-		canShowPlayer={!!$currentEpisode}
-	/>
+    <TopBar
+        bind:viewState={$viewState}
+        canShowEpisodeList={true}
+        canShowPlayer={!!$currentEpisode}
+    />
 
-	{#if $viewState === ViewState.Player}
-		<EpisodePlayer />
-	{:else if $viewState === ViewState.EpisodeList}
-		<EpisodeList
-			episodes={displayedEpisodes}
-			showThumbnails={!selectedFeed || !selectedPlaylist}
-			on:clickEpisode={handleClickEpisode}
-			on:contextMenuEpisode={handleContextMenuEpisode}
-			on:clickRefresh={handleClickRefresh}
-			on:search={handleSearch}
-		>
-			<svelte:fragment slot="header">
-				{#if selectedFeed}
-					<span
-						class="go-back"
-						on:click={() => {
-							selectedFeed = null;
-							displayedEpisodes = latestEpisodes;
-							viewState.set(ViewState.EpisodeList);
-						}}
-					>
-						<Icon
-							icon={"arrow-left"}
-							style={{
-								display: "flex",
-								"align-items": "center",
-							}}
-							size={20}
-						/> Latest Episodes
-					</span>
-					<EpisodeListHeader
-						text={selectedFeed.title}
-						artworkUrl={selectedFeed.artworkUrl}
-					/>
-				{:else if selectedPlaylist}
-					<span
-						class="go-back"
-						on:click={() => {
-							selectedPlaylist = null;
-							displayedEpisodes = latestEpisodes;
-							viewState.set(ViewState.EpisodeList);
-						}}
-					>
-						<Icon
-							icon={"arrow-left"}
-							style={{
-								display: "flex",
-								"align-items": "center",
-							}}
-							size={20}
-						/> Latest Episodes
-					</span>
-					<div
-						style="display: flex; align-items: center; justify-content: center;"
-					>
-						<Icon
-							icon={selectedPlaylist.icon}
-							size={40}
-							clickable={false}
-						/>
-					</div>
-					<EpisodeListHeader text={selectedPlaylist.name} />
-				{:else}
-					<EpisodeListHeader text="Latest Episodes" />
-				{/if}
-			</svelte:fragment>
-		</EpisodeList>
-	{:else if $viewState === ViewState.PodcastGrid}
-		<PodcastGrid
-			{feeds}
-			playlists={displayedPlaylists}
-			on:clickPodcast={handleClickPodcast}
-			on:clickPlaylist={handleClickPlaylist}
-		/>
-	{/if}
+    {#if $viewState === ViewState.Player}
+        <EpisodePlayer />
+    {:else if $viewState === ViewState.EpisodeList}
+        <EpisodeList
+            episodes={displayedEpisodes}
+            showThumbnails={!selectedFeed || !selectedPlaylist}
+            on:clickEpisode={handleClickEpisode}
+            on:contextMenuEpisode={handleContextMenuEpisode}
+            on:clickRefresh={handleClickRefresh}
+            on:search={handleSearch}
+        >
+            <svelte:fragment slot="header">
+                {#if selectedFeed}
+                    <span
+                        class="go-back"
+                        on:click={() => {
+                            selectedFeed = null;
+                            displayedEpisodes = latestEpisodes;
+                            viewState.set(ViewState.EpisodeList);
+                        }}
+                    >
+                        <Icon
+                            icon={"arrow-left"}
+                            style={{
+                                display: "flex",
+                                "align-items": "center",
+                            }}
+                            size={20}
+                        /> Latest Episodes
+                    </span>
+                    <EpisodeListHeader
+                        text={selectedFeed.title}
+                        artworkUrl={selectedFeed.artworkUrl}
+                    />
+                {:else if selectedPlaylist}
+                    <span
+                        class="go-back"
+                        on:click={() => {
+                            selectedPlaylist = null;
+                            displayedEpisodes = latestEpisodes;
+                            viewState.set(ViewState.EpisodeList);
+                        }}
+                    >
+                        <Icon
+                            icon={"arrow-left"}
+                            style={{
+                                display: "flex",
+                                "align-items": "center",
+                            }}
+                            size={20}
+                        /> Latest Episodes
+                    </span>
+                    <div
+                        style="display: flex; align-items: center; justify-content: center;"
+                    >
+                        <Icon
+                            icon={selectedPlaylist.icon}
+                            size={40}
+                            clickable={false}
+                        />
+                    </div>
+                    <EpisodeListHeader text={selectedPlaylist.name} />
+                {:else}
+                    <EpisodeListHeader text="Latest Episodes" />
+                {/if}
+            </svelte:fragment>
+        </EpisodeList>
+    {:else if $viewState === ViewState.PodcastGrid}
+        <div style="margin-bottom: 1rem;">
+            <Text
+                placeholder="Search podcasts"
+                bind:value={podcastSearchQuery}
+                style={{ width: "100%" }}
+            />
+        </div>
+        <PodcastGrid
+            feeds={filteredFeeds}
+            playlists={displayedPlaylists}
+            on:clickPodcast={handleClickPodcast}
+            on:clickPlaylist={handleClickPlaylist}
+        />
+    {/if}
 </div>
 
 <style>
-	.podcast-view {
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-	}
+    .podcast-view {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+    }
 
-	.go-back {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding: 0.5rem;
-		gap: 0.5rem;
-		cursor: pointer;
-		margin-right: auto;
-		opacity: 0.75;
-	}
+    .go-back {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.5rem;
+        gap: 0.5rem;
+        cursor: pointer;
+        margin-right: auto;
+        opacity: 0.75;
+    }
 
-	.go-back:hover {
-		opacity: 1;
-	}
+    .go-back:hover {
+        opacity: 1;
+    }
 </style>
